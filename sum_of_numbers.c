a9d1a0710b493e02865d52a28c45610a34698d4c
Sairam52 <53938921+Sairam52@users.noreply.github.com>
2021-02-11 11:21:49 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,40 @@
-//Write a program to find the sum of n different numbers using 4 functions
+#include <stdio.h>
+
+struct points
+{
+    int a[10],result;
+}p;
+
+int input(int n)
+{
+    printf("Enter elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&p.a[i]);
+    }
+}
+int compute(int n)
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+p.a[i];
+    }  
+    return sum;
+}
+
+int display(int ans)
+{
+    printf("The sum is %d:",ans);
+  
+}
+int main() 
+{
+    int n;
+    printf("Enter the number of elements");
+    scanf("%d",&n);    
+    input(n);
+    p.result=compute(n);
+    display(p.result);
+    return 0;
+}
