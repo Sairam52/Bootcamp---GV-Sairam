55108c39ae0f7e8d5ef21644aeee2d5f90a42259
Sairam52 <53938921+Sairam52@users.noreply.github.com>
2021-02-11 11:27:19 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+    int a[10],sum=0,n;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Enter elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("The sum is: %d",sum);
+    return 0;
+}
 
