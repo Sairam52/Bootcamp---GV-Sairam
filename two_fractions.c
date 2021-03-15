bb839480bd1da0aa94bde28d2b7667dc202765aa
Sairam52 <53938921+Sairam52@users.noreply.github.com>
2021-03-15 16:34:08 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,8 +2,11 @@
 struct frac
 {
 	int num,den;
+	int numerator,denominator;
 };
+
 typedef struct frac Frac;
+
 Frac input()
 {
 	Frac f;
@@ -14,9 +17,10 @@ Frac input()
 	return f;
 }
 
-int compute_sum(Frac p1, Frac p2)
+Frac compute_sum(Frac p1, Frac p2)
 {
-	int gcd, lcm,lar,numerator,denominator,big;
+	Frac f;
+	int gcd, lcm,lar,big;
 	for(int i=1;i<=p1.den && i<=p2.den;i++)
 	{
 		gcd=i;
@@ -25,31 +29,38 @@ int compute_sum(Frac p1, Frac p2)
 	if(p1.den>p2.den && p1.den%p2.den==0)
 	{
 		lar=lcm/p2.den;
-		numerator=p2.num*lar+p1.num;
-		denominator=p1.den;		
+		f.numerator=p2.num*lar+p1.num;
+		f.denominator=p1.den;		
 	}
 	
 	else if(p2.den>p1.den && p2.den%p1.den==0)
 	{
 		lar=lcm/p1.den;
-		numerator=p1.num*lar+p2.num;
-		denominator=p2.den;		
+		f.numerator=p1.num*lar+p2.num;
+		f.denominator=p2.den;		
 	}
 	
 	else
 	{
-		numerator=p2.num*p1.den+p1.num*p2.den;
-		denominator=p2.den*p1.den;
+		f.numerator=p2.num*p1.den+p1.num*p2.den;
+		f.denominator=p2.den*p1.den;
 	}
-	printf("The answer is: %d/%d",numerator,denominator);
+	return f;
+	
+}
+
+void output(Frac result, Frac f1, Frac f2)
+{
+    printf("The answer of %d/%d + %d/%d is: %d/%d",f1.num,f1.den,f2.num,f2.den,result.numerator,result.denominator);
 }
 
 int main()
 {
-	Frac f1,f2;
+	Frac f1,f2,result;
 	f1=input();
 	f2=input();
-	compute_sum(f1,f2);
+	result=compute_sum(f1,f2);
+	output(result,f1,f2);
 	return 0;
 	
 }
